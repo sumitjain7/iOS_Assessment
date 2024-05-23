a862745d451c6920ccb0403d4976b9e174537a12
Yuchen Nie <ynie@salesforce.com>
2024-05-23 15:00:45 -0600

    Removed TCA, and updated readme

--- SwiftUIIntergrationProject/Environment.swift
+++ SwiftUIIntergrationProject/Environment.swift
@@ -7,11 +7,9 @@
 
 import Foundation
 import ReactiveSwift
-import CombineSchedulers
 
 public struct Environment {
   var scheduler: ReactiveSwift.Scheduler = QueueScheduler(qos: .userInitiated, name: "userInitiated")
-  var combineScheduler: AnySchedulerOf<DispatchQueue> = .main
   var backgroundScheduler: DateScheduler = QueueScheduler(qos: .background, name: "background")
   var runLoop: RunLoop = .main
   var weatherServiceReactive: WeatherService = .live
